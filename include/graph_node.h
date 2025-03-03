79acb1b08bcbf16d406b37ec55291cadd4040b4f
Anirudh Konidala <kidskodingclub@gmail.com>
2025-03-03 10:08:39 -0600

    added a rough graph implementation

---
+++ include/graph_node.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH_NODE_H
+#define GRAPH_NODE_H
+
+#include <set>
+
+template <typename T>
+class GraphNode {
+public:
+    T value;
+
+    GraphNode(T value) : value(value) {}
+};
+
+#endif //GRAPH_NODE_H
