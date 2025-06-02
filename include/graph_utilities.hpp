642167122e356fef21d2451169e1926160db7c59
Anirudh Konidala <kidskodingclub@gmail.com>
2025-06-01 23:09:31 -0500

    test commit for gh profile readme

--- include/graph_utilities.hpp
+++ include/graph_utilities.hpp
@@ -2,10 +2,25 @@
 #define GRAPH_UTILITIES_H
 
 #include "graph.hpp"
+#include <algorithm>
 #include <map>
 #include <utility>
+#include <vector>
 
 namespace graph_utilities {
+    // test comment for gh profile readme
+
+    template <typename T>
+    int getEdges(Graph<T>& graph) {
+        int nodes = graph.graph.size();
+        return nodes * (nodes - 1) / 2;
+    }
+
+    template <typename T>
+    int getNodes(Graph<T>& graph) {
+        return graph.graph.size();
+    }
+
     template <typename T>
     std::map<GraphNode<T>*, int> getDegrees(Graph<T>& graph) {
         std::map<GraphNode<T>*, int> map;
@@ -20,6 +35,35 @@ namespace graph_utilities {
     template <typename T>
     int getDegree(GraphNode<T>*& node) {
         return node->neighbors.size();
+    }
+
+    template <typename T>
+    bool isValidGraph(Graph<T>& graph) {
+        auto degrees = graph_utilities::getDegrees(graph);
+        int sum = 0;
+        for(auto& pair : degrees) {
+            sum += pair.second;
+        }
+        return sum % 2 == 0;
+    }
+
+    template <typename T>
+    std::vector<std::vector<GraphNode<T>*>> color(Graph<T>& graph) {
+        std::vector<std::vector<GraphNode<T>*>> list;
+        auto map = graph_utilities::getDegrees(graph); 
+        std::vector<int> degrees;
+        for(const auto& pair : map) {
+            degrees.push_back(pair.second);
+        }
+
+        int i = degrees.size() - 1;
+        while(i >= 0) {
+            int maxDegree = *std::max_element(degrees.begin(), degrees.end());
+            // find the key associated with maxDegree in map
+            i -= 1;
+        }
+
+        return list;
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,3 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
 int main() {
-    std::cout << "hello world!" << "\n";
     return 0;
-}
-
-std::vector<std::string> test() {
-    std::vector<std::string> test = {"fun", "fun"};
-    return test;
 }
